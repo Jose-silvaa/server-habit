9102e0a0ccc2235a8848ae8fda238c06164b8644
Jose-silvaa <jvitorfacanha@gmail.com>
2025-01-09 20:10:38 -0300

    Add hash-password and email-validation

--- src/controllers/user/userController.ts
+++ src/controllers/user/userController.ts
@@ -1,4 +1,6 @@
+import { FastifyRequest } from 'fastify/types/request';
 import * as userService from '../../services/userService'
+import { FastifyReply } from 'fastify';
 
 export const getUserByIdController = async (req:any, res:any) => {
     const { id } = req.params;
@@ -15,14 +17,27 @@ export const getUserByIdController = async (req:any, res:any) => {
     }
   };
   
-export const createUserController = async (req:any, res:any) => {
+export const createUserController = async (req: FastifyRequest, res: FastifyReply) => {
 
-    const { email, password } = req.body;
+    const { name, email, password } = req.body as { name: string; email: string; password: string };
+    
+    if (!name || !email || !password) {
+      return res.code(400).send({ message: 'All fields are required: name, email, password' });
+    }
 
     try {
-      const newUser = await userService.createUserService({ email, password });
+
+      const validateEmail = await userService.verifyEmailUserService(email);
+
+      if(validateEmail){
+        res.code(400).send({message : 'Email Already registred'})
+      }
+
+      const hashedPassword = await req.server.bcrypt.hash(password);
+
+      const newUser = await userService.createUserService({ name, email, password : hashedPassword});
       res.code(201).send(newUser);
     } catch (error: any) {
-      res.code(500).send({ message: 'Internal Server Error' , error : error.message});
+      res.code(500).send({ message: 'An error occurred while creating the user' , error : error.message});
     }
 };
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,17 @@ import fastify from 'fastify'
 import { userRoutes } from './routes/users/userRoutes';
 import { cueRoutes } from './routes/cue/cueRoutes';
 import { cravingRoutes } from './routes/craving/cravingRoutes';
+import fastifyBcrypt from 'fastify-bcrypt';
 
 const server = fastify();
 
+server.register(fastifyBcrypt, {
+  saltWorkFactor : 12
+})
 server.register(userRoutes);
 server.register(cueRoutes);
 server.register(cravingRoutes)
+
 
 const start = async () => {
     try {
